617d564a6f47e222443a4585c88ca402ff59a11d
Oops <96kite@hanmail.net>
2017-08-01 13:18:59 +0900

    change bottomNaviAttr

--- app/src/main/java/com/mrhi2017/widget/MainActivity.java
+++ app/src/main/java/com/mrhi2017/widget/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setItemIconTintList(null);
     }
 
 }
